cb122aefb3249d63be0c0155480f4480e51c41f8
ZeZeRoN613 <68730688+ZeZeRoN613@users.noreply.github.com>
2020-08-04 10:46:28 +0300

    Add files via upload

---
+++ amcama hesap makinesi.py
@@ -0,0 +1,23 @@
+print=(banner)"""
+######################################################
+#-------------------SAYIN EYUP OZCAN-----------------#
+######################################################
+"""
+
+print("HOSGELDINIZ")
+a=input("SAYI GIR AMCA:   ")
+print("kullanicinin girdigi deger:",a)
+
+
+"""
+CIKTI
+
+SAYI GIR AMCA:25
+kullanicinin girdigi deger:25
+"""
+
+banner"""
+#################
+####HOSCAKALIN###
+#################
+"""
